285e83d019fc9bbafbe859dcbe5691c6a2015a88
Mojtaba Valipour <mojtaba.valipour@uwaterloo.ca>
2021-04-29 23:43:52 -0400

    Fix: a bug about the confusion of model_type and model_size argument has been resolved; This bug leads to not usage of the pointNet

--- train_tpu_adafactor_8core.sh
+++ train_tpu_adafactor_8core.sh
@@ -2,28 +2,26 @@
 
 export PYTHONPATH=/home/m5valipo/symbolicGPT2
 
-learning_rate=1e-4
-init_checkpoint=""
+learning_rate=1e-4init_checkpoint=""
 max_seq_length=1024
-save_checkpoint_steps=1000
-gradient_accumulation=5 #8 #16
+save_checkpoint_steps=1000gradient_accumulation=5 #8 #16
 numberofPoints=30 
 numberofVars=1
 model_type="PT" # GPT2
 
 # You can customize the training here
 # mega, medium, or base
-model_type="base"
-OUTPUT_DIR="gs://persian-storage/expSymbolic/Mesh_Simple_GPT2_1024_Sorted_PointNet/${model_type}/" # put your output directory here
+model_size="base"
+OUTPUT_DIR="gs://persian-storage/expSymbolic/Mesh_Simple_GPT2_1024_Sorted_${model_type}/${model_size}/" # put your output directory here
 input_file="gs://persian-storage/symbolic/meshGPT1024SortedPT/*.tfrecord" # put your input files here, it can also be something like "*.tfrecord"
 
-if [ ${model_type} == "base" ]; then
+if [ ${model_size} == "base" ]; then
     num_tpu_cores=8 #32
     batch_size_per_core=13 #16
-elif [ ${model_type} == "large" ]; then
+elif [ ${model_size} == "large" ]; then
     num_tpu_cores=8 #128
     batch_size_per_core=4 #16 #4
-elif [ ${model_type} == "mega" ]; then
+elif [ ${model_size} == "mega" ]; then
     num_tpu_cores=256
     batch_size_per_core=2
 fi
@@ -37,7 +35,7 @@ trap "exit" INT
 while true
 do
     timeout --foreground --signal=SIGKILL 11h python train.py \
-    --config_file=configs/${model_type}.json \
+    --config_file=configs/${model_size}.json \
     --input_file=${input_file} \
     --output_dir=${OUTPUT_DIR} \
     --max_seq_length=${max_seq_length} \
@@ -47,14 +45,14 @@ do
     --num_warmup_steps=10000 \
     --save_checkpoints_steps=${save_checkpoint_steps} \
     --iterations_per_loop=${save_checkpoint_steps} \
-    --use_tpu=True \
-    --tpu_name=$TPU_NAME\
+    --use_tpu=True \    
+    --tpu_name=$TPU_NAME\    
     --num_tpu_cores=$num_tpu_cores \
     --init_checkpoint=${init_checkpoint}\
-    --gradient_accumulation=${gradient_accumulation}\
+    --gradient_accumulation=${gradient_accumulation}\    
     --numberofPoints=${numberofPoints}\
     --numberofVars=${numberofVars}\
     --model_type=${model_type}
-    echo restarting
+    echo restarting   
     sleep 30
 done
