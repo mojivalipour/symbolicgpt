6ffcdb626abcde339fe2bc0407dd1d8c28669569
Mojtaba Valipour <mojtaba.valipour@uwaterloo.ca>
2021-05-07 17:56:07 -0400

    Fix: didn't consider that the logit is flat already, needed to ignore the first token for all samples in the whole batch

--- modeling.py
+++ modeling.py
@@ -716,8 +716,10 @@ class GroverModel(object):
         # [batch_size * seq_length, vocab_size]
 
         if self.logits_flat.shape[1] == self.seq_length+1: 
+            # self.logits_flat: self.batch_size * self.seq_length, self.config.hidden_size
+            #self.logits_flat = tf.reshape(self.logits_flat, [self.batch_size, self.seq_length, self.config.hidden_size])
             ## ignore the pointNET additional input for the prediction
-            self.logits_flat = self.logits_flat[:,1:,:]
+            self.logits_flat = self.logits_flat[self.batch_size+1:,:]
 
         logprobs_flat = tf.nn.log_softmax(self.logits_flat, axis=-1) 
 
