Fix: some overflow control and other minor issues

generator/treeBased/dataset.py
@@ -110,16 +110,16 @@ def main():
     np.random.seed(seed=seed) # fix the seed for reproducibility
 
     #NOTE: For linux you can only use unique numVars, in Windows, it is possible to use [1,2,3,4] * 10!
-    numVars = [1,2] #list(range(31)) #[1,2,3,4,5]
+    numVars = [1] #list(range(31)) #[1,2,3,4,5]
     decimals = 2
-    numberofPoints = [20,21] # only usable if support points has not been provided
+    numberofPoints = [30,31] # only usable if support points has not been provided
     numSamples = 10000 # number of generated samples
     folder = './Dataset'
     dataPath = folder +'/{}_{}_{}.json'
 
     testPoints = False
-    xRange = [-1.0,1.0]
-    testRange = [[-5.0, 1.0],[-1.0, 5.0]] # this means Union((-5,-1),(1,5))
+    xRange = [-3.0,3.0]
+    testRange = [[-5.0, 3.0],[-3.0, 5.0]] # this means Union((-5,-1),(1,5))
 
     supportPoints = None
     #supportPoints = np.linspace(xRange[0],xRange[1],numberofPoints[1])
@@ -134,16 +134,17 @@ def main():
     
     n_levels = 4
     allow_constants = True
-    const_range = [-1, 1]
+    const_range = [-2.1, 2.1]
     const_ratio = 0.5
     op_list=[
                 "id", "add", "mul",
-                "sin", "pow", "cos", "log", #"exp", "div", "sub", 
+                "sin", "pow", "cos", 
+                "exp", "div", "sub", "log"
             ]
     exponents=[3, 4, 5, 6]
 
     sortY = False # if the data is sorted based on y
-    numSamplesEachEq = 10
+    numSamplesEachEq = 50
 
     print(os.mkdir(folder) if not os.path.isdir(folder) else 'We do have the path already!')
 

symbolicGPT.py
@@ -35,18 +35,18 @@ from utils import processDataFiles, CharDataset, relativeErr, mse, sqrt, divide,
 set_seed(42)
 
 # config
-numEpochs = 20 # number of epochs to train the GPT+PT model
+numEpochs = 2 # number of epochs to train the GPT+PT model
 embeddingSize = 512 # the hidden dimension of the representation of both GPT and PT
-numPoints=20 # number of points that we are going to receive to make a prediction about f given x and y, if you don't know then use the maximum
-numVars=2 # the dimenstion of input points x, if you don't know then use the maximum
+numPoints=30 # number of points that we are going to receive to make a prediction about f given x and y, if you don't know then use the maximum
+numVars=1 # the dimenstion of input points x, if you don't know then use the maximum
 numYs=1 # the dimension of output points y = f(x), if you don't know then use the maximum
 blockSize = 100 # spatial extent of the model for its context
 batchSize = 128 # batch size of training data
 dataDir = './datasets/'
-dataInfo = 'XYE_1-{}Var_{}Points_{}EmbeddingSize'.format(numVars, numPoints, embeddingSize)
-titleTemplate = "{} equations of 1-{} variables - Benchmark"
+dataInfo = 'XYE_{}Var_{}Points_{}EmbeddingSize'.format(numVars, numPoints, embeddingSize)
+titleTemplate = "{} equations of {} variables - Benchmark"
 target = 'Skeleton' #'Skeleton' #'EQ'
-dataFolder = '1-2Var_RandSupport_RandLength_-1to1_-5to1_1to5_100to500Points'
+dataFolder = '1Var_RandSupport_FixedLength_-3to3_-6.1to-3.0-3.1to6_30Points'
 addr = './SavedModels/' # where to save model
 method = 'EMB_SUM' # EMB_CAT/EMB_SUM/OUT_SUM/OUT_CAT/EMB_CON -> whether to concat the embedding or use summation. 
 # EMB_CAT: Concat point embedding to GPT token+pos embedding
@@ -79,7 +79,7 @@ text = processDataFiles(files)
 chars = sorted(list(set(text))+['_','T','<','>',':']) # extract unique characters from the text before converting the text to a list, # T is for the test data
 text = text.split('\n') # convert the raw text to a set of examples
 text = text[:-1] if len(text[-1]) == 0 else text
-random.shuffle(text) # shuffle the dataset, it's important for combined number of variables
+random.shuffle(text) # shuffle the dataset, it's important specailly for the combined number of variables experiment
 train_dataset = CharDataset(text, blockSize, chars, numVars=numVars, 
                 numYs=numYs, numPoints=numPoints, target=target, addVars=addVars) 
 
@@ -144,10 +144,10 @@ tconf = TrainerConfig(max_epochs=numEpochs, batch_size=batchSize,
                       num_workers=0, ckpt_path=ckptPath)
 trainer = Trainer(model, train_dataset, val_dataset, tconf, bestLoss)
 
-try:
-    trainer.train()
-except KeyboardInterrupt:
-    print('KeyboardInterrupt')
+# try:
+#     trainer.train()
+# except KeyboardInterrupt:
+#     print('KeyboardInterrupt')
 
 # load the best model
 model.load_state_dict(torch.load(ckptPath))
@@ -206,13 +206,14 @@ try:
             o.write('{}\n'.format(predicted))
 
             # train a regressor to find the constants (too slow)
-            c = [1 for i,x in enumerate(predicted) if x=='C']            
-            
+            c = [1.0 for i,x in enumerate(predicted) if x=='C'] # initialize coefficients as 1
+            c[-1] = 0 # initialize the constant as zero
+            b = [(-2,2) for i,x in enumerate(predicted) if x=='C']  # bounds on variables
             try:
                 if len(c) != 0:
                     # This is the bottleneck in our algorithm
                     # for easier comparison, we are using minimize package  
-                    cHat = minimize(lossFunc, c,
+                    cHat = minimize(lossFunc, c, #bounds=b,
                                    args=(predicted, t['X'], t['Y'])) 
         
                     predicted = predicted.replace('C','{}').format(*cHat.x)

utils.py
@@ -63,6 +63,10 @@ def sqrt(x):
   x = np.nan_to_num(x)
   return np.sqrt(np.abs(x)) 
 
+def log(x):
+  x = np.nan_to_num(x)
+  return np.log(np.abs(x)) 
+
 # Mean square error
 def mse(y, y_hat):
     y_hat = np.reshape(y_hat, [1, -1])[0]
@@ -106,7 +110,7 @@ class CharDataset(Dataset):
         self.paddingID = self.stoi[self.paddingToken]
         self.stoi[self.paddingToken] = self.paddingID
         self.itos[self.paddingID] = self.paddingToken
-        self.threshold = [-1000,1000]
+        self.threshold = [-10,10]
         
         self.block_size = block_size
         self.vocab_size = vocab_size
@@ -128,7 +132,7 @@ class CharDataset(Dataset):
             
         # find the number of variables in the equation
         eq = chunk[self.target]
-        vars = re.finditer('x[\d]*',eq) 
+        vars = re.finditer('x[\d]+',eq) 
         numVars = 0
         for v in vars:
             v = v.group(0).strip('x')
@@ -165,7 +169,7 @@ class CharDataset(Dataset):
             p = x+y # because it is only one point 
             p = torch.tensor(p)
             #replace nan and inf
-            p = torch.nan_to_num(p, nan=0.0, 
+            p = torch.nan_to_num(p, nan=self.threshold[1], 
                                  posinf=self.threshold[1], 
                                  neginf=self.threshold[0])
             p[p>self.threshold[1]] = self.threshold[1] # clip the upper bound
@@ -193,6 +197,8 @@ def lossFunc(constants, eq, X, Y):
 
     for x,y in zip(X,Y):
         eqTemp = eq + ''
+        if type(x) == np.float32:
+                x = [x]
         for i,e in enumerate(x):
             # make sure e is not a tensor
             if type(e) == torch.Tensor:
@@ -203,6 +209,11 @@ def lossFunc(constants, eq, X, Y):
         except:
             print('Exception has been occured! EQ: {}, OR: {}'.format(eqTemp, eq))
             yHat = 100
-        err += (y-yHat)**2
+        try:
+            # handle overflow
+            err += (y-yHat)**2
+        except:
+            print('Exception has been occured! EQ: {}, OR: {}, y:{}-yHat:{}'.format(eqTemp, eq, y, yHat))
+            err = 100
     err /= len(Y)
     return err