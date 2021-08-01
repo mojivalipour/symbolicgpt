3837a68f2dbdc259d2ad771adad05ee5bb2b4308
Mojtaba Valipour <mojtaba.valipour@uwaterloo.ca>
2021-08-01 05:12:45 -0400

    Feat: add Nucleus sampling

--- utils.py
+++ utils.py
@@ -21,8 +21,70 @@ def top_k_logits(logits, k):
     out[out < v[:, [-1]]] = -float('Inf')
     return out
 
+# @torch.no_grad()
+# def sample(model, x, steps, points=None, variables=None, temperature=1.0, sample=False, top_k=None):
+#     """
+#     take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
+#     the sequence, feeding the predictions back into the model each time. Clearly the sampling
+#     has quadratic complexity unlike an RNN that is only linear, and has a finite context window
+#     of block_size, unlike an RNN that has an infinite context window.
+#     """
+#     block_size = model.get_block_size()
+#     model.eval()
+#     for k in range(steps):
+#         x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+#         logits, _ = model(x_cond, points=points, variables=variables)
+#         # pluck the logits at the final step and scale by temperature
+#         logits = logits[:, -1, :] / temperature
+#         # optionally crop probabilities to only the top k options
+#         if top_k is not None:
+#             logits = top_k_logits(logits, top_k)
+#         # apply softmax to convert to probabilities
+#         probs = F.softmax(logits, dim=-1)
+#         # sample from the distribution or take the most likely
+#         if sample:
+#             ix = torch.multinomial(probs, num_samples=1)
+#         else:
+#             _, ix = torch.topk(probs, k=1, dim=-1)
+#         # append to the sequence and continue
+#         x = torch.cat((x, ix), dim=1)
+
+#     return x
+
+#use nucleus sampling from https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+def top_k_top_p_filtering(logits, top_k=0.0, top_p=0.0, filter_value=-float('Inf')):
+    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+        Args:
+            logits: logits distribution shape (vocabulary size)
+            top_k >0: keep only top k tokens with highest probability (top-k filtering).
+            top_p >0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+        
+        Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+    """
+    #TODO: support for batch size more than 1
+    assert logits.dim() == 1  # batch size 1 for now - could be updated for more but the code would be less clear
+    top_k = min(top_k, logits.size(-1))  # Safety check
+    if top_k > 0:
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        logits[indices_to_remove] = filter_value
+
+    if top_p > 0.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold
+        sorted_indices_to_remove = cumulative_probs > top_p
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        indices_to_remove = sorted_indices[sorted_indices_to_remove]
+        logits[indices_to_remove] = filter_value
+    return logits
+
 @torch.no_grad()
-def sample(model, x, steps, points=None, variables=None, temperature=1.0, sample=False, top_k=None):
+def sample(model, x, steps, points=None, variables=None, temperature=1.0, sample=False, top_k=0.0, top_p=0.0):
     """
     take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
     the sequence, feeding the predictions back into the model each time. Clearly the sampling
@@ -35,10 +97,11 @@ def sample(model, x, steps, points=None, variables=None, temperature=1.0, sample
         x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
         logits, _ = model(x_cond, points=points, variables=variables)
         # pluck the logits at the final step and scale by temperature
-        logits = logits[:, -1, :] / temperature
+        logits = logits[0, -1, :] / temperature
         # optionally crop probabilities to only the top k options
-        if top_k is not None:
-            logits = top_k_logits(logits, top_k)
+#         if top_k is not None:
+#             logits = top_k_logits(logits, top_k)
+        logits = top_k_top_p_filtering(logits, top_k=top_k, top_p=top_p)
         # apply softmax to convert to probabilities
         probs = F.softmax(logits, dim=-1)
         # sample from the distribution or take the most likely
@@ -47,7 +110,7 @@ def sample(model, x, steps, points=None, variables=None, temperature=1.0, sample
         else:
             _, ix = torch.topk(probs, k=1, dim=-1)
         # append to the sequence and continue
-        x = torch.cat((x, ix), dim=1)
+        x = torch.cat((x, ix.unsqueeze(0)), dim=1)
 
     return x
 
